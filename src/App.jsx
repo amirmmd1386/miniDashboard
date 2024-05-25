4e8b8e38d3ae06e4b4b42053ba101d245420f61b
amirmmd1386 <hajamir1386.0@gmail.com>
2024-05-25 11:32:21 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import React, { useContext, useState } from 'react';
-import AddList from './AddToList';
-import ListItem from './list';
-import Listcontext from './contextList';
+import React from 'react';
+import { Sidebar } from './sidebar';
+import  User  from './components/user/users';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Work } from './components/work/work';
+import { Gallery } from './components/gallery/gallery';
+import { Post } from './components/post/post';
+import { Add } from './components/user/formEditAdd';
 const App = () => {
-    const [work, setWork] = useState([
-    ])
     return (
-        <>
-            <Listcontext.Provider value={{
-                work , setWork
-            }}>
-                <div>
-                    <div><h1>MY SECOND REACT PROJECT TO_DO_LIST</h1></div>
-                    <div className='container'>
-                        <AddList />
-                        <ListItem />
-                    </div>
-                </div>
-            </Listcontext.Provider>
-        </>
+       <div className='d-flex flex-column justify-content-center align-items-center w-100 overflow-hidden text-white '>
+    
+            <BrowserRouter>
+                <Sidebar />
+                <Routes>
+                    <Route path='/' element={<User />} />
+                    <Route path='/user/add' element={<Add />} >
+                        <Route path=':userId' />
+                    </Route>
+                    <Route path='/work' element={<Work />} />
+                    <Route path='/gallery' element={<Gallery />} />
+                    <Route path='/post' element={<Post />} />
+                    <Route path='*' element={<User />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 }
 export default App
--- src/contextList.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-const Listcontext = createContext({
-    work: [],
-    setWork: () => { }
-})
-export default Listcontext
